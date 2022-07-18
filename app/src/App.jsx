2829ee361179f39f3a6fd7e651b40a69896c7c21
Dmitry Bis <26793601+zerthmonk@users.noreply.github.com>
2022-07-19 00:43:47 +0400

    Store в минимальной конфигурации с помощью slice и redux-toolkit (#3)

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import { Main } from './pages/Main/Main';
+import { Offline } from './pages/Offline/Offline'
+import { useSelector, Provider } from 'react-redux';
+import { store } from './store';
 
 const App = () => {
+  const modes = useSelector(state => state.modes);
+  const config = useSelector(state => state.config);
+
+  const currentMode = modes[config.current] !== true || modes[config.default];
+
   return (
-    <>
-      <Main/>
-    </>
+    currentMode.power ? <Main/> : <Offline/>
   );
 }
 
 export const renderApp = (root) => {
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>
   );
 }
