50f52e8d580f68e4ebc2025187b4c9589915035a
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-07-18 13:58:34 +0400

    styles fix

--- app/src/components/console/ConsoleOutput/ConsoleOutput.jsx
+++ app/src/components/console/ConsoleOutput/ConsoleOutput.jsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import classnames from 'classnames';
 import styles from './styles.module.scss';
 
-export const ConsoleOutput = ({rows, className}) => {
+export const ConsoleOutput = ({rows}) => {
   return (
-    <div className={classnames(styles.root, className)}>
-      <div className={styles.output}>
-        {rows?.length ? rows.map(r => <p>{r}</p>) : ""}
-      </div>
-      <div className={styles.input}></div>
+    <div className={styles.root}>
+      {rows?.length ? rows.map(r => <p>{r}</p>) : ""}
     </div>
   )
 }
