a963e05bae07cc02b4fdae5caf8d8321b97492f0
Dmitry Bis <26793601+zerthmonk@users.noreply.github.com>
2022-07-23 04:23:48 +0400

    Правки верстки экрана логина для десктопа (#7)
    * Спавн окна логина при выборе пользователя
    
    * Ассеты
    
    * Доработана кнопка
    
    * Добавлен кастомный хук для отслеживания клика мимо компонента
    
    * Правки стилей

--- app/src/components/login/Login/Login.jsx
+++ app/src/components/login/Login/Login.jsx
@@ -1,32 +1,53 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import classnames from 'classnames';
 import { getUsers } from 'Features/modes/selectors';
 import { LoginBox } from 'Components/login/LoginBox/LoginBox.jsx';
 import { Button } from 'Components/base/Button/Button';
+import useComponentVisible from 'Helpers/useComponentVisible.js';
 import styles from './styles.module.scss';
 
 export const Login = ({ className }) => {
   const users = getUsers();
-  const [currentUser, setCurrentUser] = useState('');
-  const handleSelect = (userName) => {
-    return setCurrentUser(userName);
+  const [ currentUser, setCurrentUser ] = useState('');
+  const { ref, isComponentVisible, setIsComponentVisible } = useComponentVisible(false);
+
+  useEffect(() => { 
+    if (currentUser !== '') {
+      setIsComponentVisible(true)
+    }
+  }, [currentUser])
+
+  useEffect(() => {
+    if (!isComponentVisible) {
+      setCurrentUser('');
+    }
+  }, [isComponentVisible])
+
+  const handleClick = (username) => {
+    if (currentUser !== username) {
+      setCurrentUser(username);
+    }
   }
 
   return (
     <div className={classnames(styles.root, className)}>
       <div>
         <div className={styles.users}>
-          {Object.entries(users).map(([username, data]) => {
-            return <Button key={username} text={data?.name || 'unknown user'} 
+          {Object.entries(users).map(([username, data], idx) => {
+            return <Button key={username}
+                           text={data?.name || 'unknown user'}
+                           tabIndex={idx + 1}
                            className={classnames(styles.row, {[styles.hacked]: data.hacked})}
-                           onClick={() => handleSelect(username)}/>
+                           onClick={() => handleClick(username)}/>
           })}
         </div>
       </div>
-      <div className={styles.login}>
-        <LoginBox {...users?.[currentUser]}/>
-      </div>
+      {isComponentVisible && users?.[currentUser] &&
+        <div ref={ref} className={styles.login}>
+          <LoginBox username={currentUser} userdata={users?.[currentUser]}/>
+        </div>
+      }
     </div>
   )
 }
--- app/src/components/login/LoginBox/LoginBox.jsx
+++ app/src/components/login/LoginBox/LoginBox.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React from 'react';
+import classnames from 'classnames';
+import NetworkSvg from 'Assets/svg/network.svg';
+import styles from './styles.module.scss';
+import { Button } from 'Components/base/Button/Button.jsx';
+import { RandomizeText } from 'Components/effects/RandomizeText/RandomizeText.jsx';
 
-export const LoginBox = ({name, hackable}) => {
+export const LoginBox = ({ username, userdata={} }) => {
+
+  const animateRate = 300;
+  const headerLabel = 'login attempt';
+  const passwordLabel = 'password';
+  const { name, hacked } = {...userdata};
+
   return (
-    <div>{name}</div>
+    <div className={classnames(styles.root, {[styles.hacked]: hacked})}>
+      <div className={classnames(styles.header, styles.label)}>
+        <RandomizeText value={`/// ${headerLabel} : ${username} // ${hacked ? 'compromised' : 'secure'}`} rate={animateRate}/>
+      </div>
+      <div className={styles.details}>
+        <NetworkSvg className={styles.logo}/>
+        <div className={classnames(styles.row, styles.title)}>
+          <span className={styles.label}>user account:</span>
+          <span className={styles.title__name}>{name}</span>
+        </div>
+        <div className={classnames(styles.row, styles.password)}>
+          <span className={styles.label}>password:</span>
+          <input type="password" autoFocus></input>
+        </div>
+        <div className={styles.controls}>
+          <div className={styles.control}><span>RECOVERY</span></div>
+          <div className={styles.control}><span>SUBMIT</span></div>
+        </div>
+      </div>
+    </div>
   )
 }
