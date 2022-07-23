9ed00ee290cad662c8e6ae6e4332fb524a704b38
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2022-07-23 04:20:18 +0400

    Правки стилей

--- app/src/components/login/LoginBox/LoginBox.jsx
+++ app/src/components/login/LoginBox/LoginBox.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import classnames from 'classnames';
 import NetworkSvg from 'Assets/svg/network.svg';
 import styles from './styles.module.scss';
+import { Button } from 'Components/base/Button/Button.jsx';
+import { RandomizeText } from 'Components/effects/RandomizeText/RandomizeText.jsx';
 
-export const LoginBox = ({ username, userdata={}, visible=false }) => {
+export const LoginBox = ({ username, userdata={} }) => {
 
-  const { name, hacked } = {...userdata};
+  const animateRate = 300;
   const headerLabel = 'login attempt';
   const passwordLabel = 'password';
+  const { name, hacked } = {...userdata};
 
   return (
     <div className={classnames(styles.root, {[styles.hacked]: hacked})}>
       <div className={classnames(styles.header, styles.label)}>
-        <span>/// {headerLabel} : {username} // {hacked ? 'compromised' : 'secure'}</span>
-        <span>---</span>
+        <RandomizeText value={`/// ${headerLabel} : ${username} // ${hacked ? 'compromised' : 'secure'}`} rate={animateRate}/>
       </div>
       <div className={styles.details}>
         <NetworkSvg className={styles.logo}/>
@@ -24,6 +26,10 @@ export const LoginBox = ({ username, userdata={}, visible=false }) => {
         <div className={classnames(styles.row, styles.password)}>
           <span className={styles.label}>password:</span>
           <input type="password" autoFocus></input>
+        </div>
+        <div className={styles.controls}>
+          <div className={styles.control}><span>RECOVERY</span></div>
+          <div className={styles.control}><span>SUBMIT</span></div>
         </div>
       </div>
     </div>
